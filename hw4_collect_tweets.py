f180350cb7d87d0376339ecc495fe07a3ee9098e
Arseny Sher <arsv.sher@gmail.com>
2016-04-23 12:47:41 +0300

    hw4 done

--- hw4_collect_tweets.py
+++ hw4_collect_tweets.py
@@ -11,6 +11,7 @@ from sklearn.feature_extraction import DictVectorizer
 import time
 import pprint
 import twitter
+import cPickle
 
 import main
 from main import log_info, log_warn
@@ -27,19 +28,21 @@ class TweetsGetter(object):
         self.black_list_ids = []
         if os.path.isfile(self.PICKLED_TWEETS):
             with open(self.PICKLED_TWEETS, 'rb') as f:
-                self.tweets = pickle.load(f)
+                self.tweets = cPickle.load(f)
         if os.path.isfile(self.BLACK_LISTED_IDS):
             with open(self.BLACK_LISTED_IDS, 'rb') as f:
-                self.black_list_ids = pickle.load(f)
-        log_info("number of users with tweets cached: %s " % len(self.tweets))
+                self.black_list_ids = cPickle.load(f)
+        self.tweets_cached = len(self.tweets)
+        log_info("number of users with tweets cached: %s " % self.tweets_cached)
         log_info("number of black listed users cached: %s " % len(self.black_list_ids))
 
     def __del__(self):
-        log_info("Saving downloaded tweets and blacklist ids")
-        with open(self.PICKLED_TWEETS, 'wb') as f:
-            pickle.dump(self.tweets, f, pickle.HIGHEST_PROTOCOL)
-        with open(self.BLACK_LISTED_IDS, 'wb') as f:
-            pickle.dump(self.black_list_ids, f, pickle.HIGHEST_PROTOCOL)
+        if len(self.tweets) > self.tweets_cached:
+            log_info("Saving downloaded tweets and blacklist ids")
+            with open(self.PICKLED_TWEETS, 'wb') as f:
+                cPickle.dump(self.tweets, f, pickle.HIGHEST_PROTOCOL)
+            with open(self.BLACK_LISTED_IDS, 'wb') as f:
+                cPickle.dump(self.black_list_ids, f, pickle.HIGHEST_PROTOCOL)
 
     # returns None in case of problems
     def get_tweets(self, user_id):
@@ -49,42 +52,51 @@ class TweetsGetter(object):
             return None
         else:
             log_info("Retrieving tweets of user with id %s..." % user_id)
-            try:
-                tweets = self.api.GetUserTimeline(user_id=user_id, count=self.NUMBER_OF_TWEETS_TO_GET,
-                                                  include_rts=False, exclude_replies=True)
-            except twitter.TwitterError, e:
-                # skip private and deleted accounts
-                if e.message == "Not authorized." or e.message == [{u'message': u'Sorry, that page does not exist.', u'code': 34}]:
-                    self.black_list_ids.append(user_id)
-                    log_info("User %s added to black list" % user_id)
-                    return None
-                elif e.message == [{u'message': u'Rate limit exceeded', u'code': 88}]:
-                    log_warn("Rate limit exceeded exception was thrown! Trying to retrieve tweets again")
-                    return self.get_tweets(user_id)
-                else:
-                    self.__del__()
-                    raise e
-            finally:
-                users_loaded = len(self.tweets)
-                if users_loaded % 50 == 0:
-                    log_info("%s users loaded, spilling them to disk" % users_loaded)
-                    self.__del__()
+            tweets = None
+            while True:
+                try:
+                    tweets = self.api.GetUserTimeline(user_id=user_id, count=self.NUMBER_OF_TWEETS_TO_GET,
+                                                      include_rts=False, exclude_replies=True)
+                except twitter.TwitterError, e:
+                    # skip private and deleted accounts
+                    if e.message == "Not authorized." or e.message == [{u'message': u'Sorry, that page does not exist.', u'code': 34}]:
+                        self.black_list_ids.append(user_id)
+                        log_info("User %s added to black list" % user_id)
+                        return None
+                    elif e.message == [{u'message': u'Rate limit exceeded', u'code': 88}]:
+                        log_warn("Rate limit exceeded exception was thrown! Trying to retrieve tweets again")
+                        continue
+                    else:
+                        self.__del__()
+                        raise e
+                except IOError:
+                    log_warn("IOError exception was thrown! No idea that it means, but trying to retreive tweets again")
+                    continue
+                finally:
+                    users_loaded = len(self.tweets)
+                    if users_loaded % 50 == 0:
+                        log_info("%s users loaded, spilling them to disk" % users_loaded)
+                        self.__del__()
+                break
 
-            self.tweets[user_id] = tweets
-            return tweets
+            self.tweets[user_id] = map(lambda tweetStatus: tweetStatus.AsDict(), tweets)
+            return self.tweets[user_id]
 
-tweets_getter = TweetsGetter()
+
 # returns None in case of problems, or list of tweets, each as a dict, otherwise
+# get_all_users_tweets is used instead
 def get_user_tweets(user_id):
-    tweets = tweets_getter.get_tweets(user_id)
-    if tweets is None:
-        return None
-    tweets_dicts = map(lambda tweetStatus: tweetStatus.AsDict(), tweets)
-    return tweets_dicts
+    pass
 
+
+def get_all_users_tweets(uids):
+    tweets_getter = TweetsGetter()
+    uids_with_tweets = map(lambda uid: (uid, tweets_getter.get_tweets(uid)), uids)
+    log_info("All tweets are loaded")
+    return uids_with_tweets
 
 # parse string to list of words, lowercasing, removing refs to other users (@usrname), reducing number of vowels,
-# ,filtering out stopwords and http(s) links
+# , filtering out http(s) links
 class Wordenizer(object):
     tokenizer = nltk.tokenize.TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 
@@ -103,7 +115,8 @@ def get_words(text):
 
 class Normalizer(object):
     lemmatizer = nltk.WordNetLemmatizer()
-    stopwords = nltk.corpus.stopwords.words()
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords += [u'every', u'wa', u'going', u"don't", u"i'm", u"via", u"really", u"time", u'would']
 
     @staticmethod
     def tokenize(word):
@@ -143,7 +156,7 @@ def tweets_to_grouped_tokens(tweets):
 
 def collect_users_tokens(df_users):
     uids = df_users["uid"].values
-    uids_with_tweets = map(lambda uid: (uid, get_user_tweets(uid)), uids)
+    uids_with_tweets = get_all_users_tweets(uids)
     uids_with_token_dict_freqs = map(lambda uid_with_tweets: (uid_with_tweets[0],
                                                               tweets_to_grouped_tokens(uid_with_tweets[1])),
                                      uids_with_tweets)
@@ -176,7 +189,7 @@ def draw_tag_cloud(users_tokens):
     import matplotlib.pyplot as plt
     from wordcloud import WordCloud, ImageColorGenerator
 
-    trump_coloring = np.array(Image.open("trump.png"))
+    trump_coloring = np.array(Image.open("pics/trump.png"))
 
     freqs = get_full_frequencies(users_tokens)
     freq_pairs = freqs.items()
@@ -186,18 +199,25 @@ def draw_tag_cloud(users_tokens):
 
     image_colors = ImageColorGenerator(trump_coloring)
 
+    # plt.imshow(wc)
+    # plt.axis("off")
+    #
+    # plt.figure()
     plt.imshow(wc.recolor(color_func=image_colors))
+    # recolor wordcloud and show
+    # we could also give color_func=image_colors directly in the constructor
+    # plt.imshow(trump_coloring, cmap=plt.cm.gray)
     plt.axis("off")
     plt.show()
 
 if __name__ == "__main__":
     df_users = main.get_users()
-    # df_users = df_users[:200]
+    df_users = df_users[:1000]
 
     users, users_tokens = collect_users_tokens(df_users)
     token_set = get_full_token_set(users_tokens)
     v = DictVectorizer()
     vs = v.fit_transform(users_tokens)
     np.savez("files/out_4.dat", data=vs, users=users, users_tokens=list(token_set))
-
+    #
     draw_tag_cloud(users_tokens)
